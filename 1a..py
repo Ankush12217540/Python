6bc031ade4d99f890229a25e0c883615972180ea
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-06 19:27:19 +0530

    Numbers

---
+++ 1a..py
@@ -0,0 +1,52 @@
+#Addition
+print(2+1)
+
+#substraction
+print(2-1)
+
+#Multiplication
+print(2*2)
+
+#Division
+print(3/2)
+
+#floor Division--->turncates the decimal without rounding and return the integer  value
+print(7//4)
+
+#power
+print(2**3)
+
+#Can also do root this way
+print(4**0.5)
+
+#Order of operation followed in Python 
+print(2+10*10+3)
+
+#Can use parentheses to specify orders
+(2+10)+(10+3)
+
+#lets create an objct called 'a' and assign it the number 5
+a=5
+
+#Adding the object
+print(a+a)
+
+#Reassignment
+a=11
+
+#check
+print(a)
+
+#Using a to redfine A
+a=a+a
+
+#Check
+print(a)
+
+#Use object names to keep better teack of what's going on in your code!
+my_income=100
+tax_rate=0.1
+my_taxes=my_income*tax_rate
+
+#Shows my my_taxes
+print(my_taxes)
