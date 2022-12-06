b2d7d54098f9cf4ae0d5bd374ed61693b4f8c173
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-06 19:27:54 +0530

    Variable

---
+++ 1b.py
@@ -0,0 +1,27 @@
+#Dynamics Typing
+my_dogs=2
+print(my_dogs)
+
+my_dogs=['Sammy','Frankie']
+print(my_dogs)
+
+#Assiging Variables
+a=5
+print(a)
+a=10
+print(a)
+print(a+a)
+
+#Reassiging Variables
+a=a+10
+print(a)
+
+#There's actually shortcuts to this.Pyhon lets you add,substract,
+# multipl and divide with reassignment using +=,-=,*=, and /=.
+a+=10
+print(a)
+a*=2
+print(a)
+print(type(a))
+a=(1,2)
+print(type(a))
