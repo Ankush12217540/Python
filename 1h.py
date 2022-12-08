e6e22a2e0d10f7389d18b49e2dea994aa17419d0
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-08 20:41:26 +0530

    Sets and Booleans

---
+++ 1h.py
@@ -0,0 +1,27 @@
+# SETS AND BOOLEANS
+
+#sets
+x=set()
+# we add to sets with the add() method
+x.add(1)
+print(x)
+# adding a different element
+x.add(2)
+print(x)
+#adding the same element
+x.add(1)
+print(x)
+# create a list with repeats
+list1=[1,1,2,2,3,4,5,6,1,1]
+#cast as set to get unique values
+print(set(list1))
+
+#  BOOLEANS
+
+#set object to be a boolean
+a=True
+print(a)
+print(2>3)
+# None placeholder
+b=None
+print(b)
