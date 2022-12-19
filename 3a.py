25ea6ddbfa18cc1d2ec6cf4a4b6efb82e1c0f457
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-19 23:35:46 +0530

    if,elif and else statement

---
+++ 3a.py
@@ -0,0 +1,30 @@
+# if,elif and else statement
+
+if True:
+    print('It was true!')
+
+x=False
+if x:
+    print('x was True!')
+else:
+    print('I will be printed in any case where x is not true')
+    
+
+
+# MULTIPLE BRANCHES
+loc='Bank'
+if loc=='Auto Shop':
+    print('welcome to the Auto Shop')
+elif loc=='Bank':
+    print('Welcome to the bank')
+else:
+    print('Where are you?')
+    
+person='George'
+if person=='Sammy':
+    print('Welcome Sammy')
+elif person=='Sammy':
+    print('Welcome George!')
+else:
+    print("Welcome, what's your name?")
+    
