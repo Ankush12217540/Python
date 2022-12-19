112e945daddaee79e83fe9cbeccfc681ec0a007f
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-19 23:33:53 +0530

    Chained Comparision Operator

---
+++ 2b.py
@@ -0,0 +1,12 @@
+#  CHAINED COMPARISION OPERATOR
+
+
+print(1<2<3)
+print(1<2 and 2<3)   
+'''The "and" is used to make sure two
+        checks have to be to be true in orer for the total checks to be true'''
+print(1<3>2)
+print(1<3 and 3>2)
+
+print(1==2 or 2<3)
+''' With the "or" operator, we only need one or the other to be true. '''
