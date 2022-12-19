ec328749d83725079e3d50c382b6e609f6489507
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-19 23:33:06 +0530

    All type of comparision operator

---
+++ 2a.py
@@ -0,0 +1,27 @@
+# COMPARISION OPERATOR
+
+# Equal
+print(2==2)
+print(1==0)
+
+# Note that '==' is a comparision operator, while '=' is an assignment operator
+
+# Not Equal
+print(2!=2)
+print(2!=1)
+
+# Greater Than
+print(2>1)
+print(2>4)
+
+# Less Than
+print(2<4)
+print(2<1)
+
+# Greater than or Equal to 
+print(2>=2)
+print(2>=1)
+
+# Less Than or Equal to 
+print(2<=2)
+print(2<=4)
