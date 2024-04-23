786cc2dc5eddb4c52b020f07fb77c56d77e18b5f
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-04-23 23:59:29 +0530

    COMPARISION OPERATOR.py

--- 2) COMPARISION OPERATOR.py
+++ 2) COMPARISION OPERATOR.py
@@ -7,7 +7,7 @@ In the table below, a=3 and b=4.
 
 Operator	Description	Example
 ==	If the values of two operands are equal, then the condition becomes true.	(a == b) is not true.
-!=	If values of two operands are not equal, then condition becomes true.	(a != b) is true
+!=	If values of two operands are not equal, then condition becomes true.	(a != b) is true. 
 >	If the value of left operand is greater than the value of right operand, then condition becomes true.	(a > b) is not true.
 <	If the value of left operand is less than the value of right operand, then condition becomes true.	(a < b) is true.
 >=	If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.	(a >= b) is not true.
